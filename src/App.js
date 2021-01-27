4289ecb198f52fc11f7e1f21f8f8f4fa7ec2d66b
Swagat Bhattacharjee <66122924+swagat9712@users.noreply.github.com>
2021-01-27 12:46:49 +0600

    Added: files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from './components/login/Login';
+import {Router, Route, Switch} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
